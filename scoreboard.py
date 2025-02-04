250ba98dd777eb56134c8871f552077f03d2d395
huyho2621 <150783343+huyho2621@users.noreply.github.com>
2025-02-04 10:50:59 +0100

    Complete

--- scoreboard.py
+++ scoreboard.py
@@ -1,17 +1,30 @@
 from turtle import Turtle
 
+from setuptools.windows_support import hide_file
+
 FONT = ("Courier", 20, "normal")
 
 
 class Scoreboard(Turtle):
+
     def __init__(self, height):
         super().__init__()
         self.score = 0
+        with open("data.txt", "r") as readfile:
+            self.high_score = int(readfile.read())
         self.color("white")
         self.hideturtle()
         self.penup()
         self.sety((height / 2) - 30)
-        self.write(f'Score: {self.score}', move=False, align="center", font=FONT)
+        self.update_score()
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", "w") as writefile:
+                writefile.write(str(self.high_score))
+        self.score = 0
+        self.update_score()
 
     def increase_score(self):
         self.score += 1
@@ -19,8 +32,8 @@ class Scoreboard(Turtle):
 
     def update_score(self):
         self.clear()
-        self.write(f'Score: {self.score}', move=False, align="center", font=FONT)
+        self.write(f'Score: {self.score} High Score: {self.high_score}', move=False, align="center", font=FONT)
 
-    def game_over(self):
-        self.home()
-        self.write("GAME OVER.", move=False, align="center", font=FONT)
+    # def game_over(self):
+    #     self.home()
+    #     self.write("GAME OVER.", move=False, align="center", font=FONT)
